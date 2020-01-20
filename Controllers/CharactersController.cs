b222a1846353fda1aad4aa9b85f0589a1fb1e918
jdesrochers <julien.desrochers@gestabec.ca>
2020-01-20 17:43:44 -0500

    Change the logic to init with DomContentLoaded

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -8,7 +8,6 @@ namespace TheStarWarsProject.Controllers
 {
     public class CharactersController : Controller
     { 
-        // GET: /Home/
         public ActionResult Index()
         {
             List<Character> characters = GetCharacters("./App_Data/Characters.json");
